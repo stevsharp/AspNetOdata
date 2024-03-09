26a03b014b64ad3929edb791fed1ba41d6f419dc
SPYROS\Spyros <sponaris@gmail.com>
2024-03-09 23:00:56 +0200

    Add Filter by Key

--- AspNetOdata/Controllers/ProductController.cs
+++ AspNetOdata/Controllers/ProductController.cs
@@ -1,8 +1,14 @@
 ﻿using AspNetOdata.Models;
 
+using Microsoft.AspNet.OData.Routing;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Formatter;
 using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Results;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+using System.Linq.Expressions;
 
 namespace AspNetOdata.Controllers
 {
@@ -21,8 +27,14 @@ namespace AspNetOdata.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-
             return Ok(_eClinicContext.Products.AsQueryable());
+        }
+
+        [ODataRoute("({key})")]
+        [EnableQuery]
+        public virtual SingleResult<Product> Get([FromODataUri] int key)
+        { 
+            return SingleResult.Create(_eClinicContext.Set<Product>().Where(x=>x.Id == key).AsQueryable());
         }
 
     }
--- BlazorAppOdataClient/Pages/Home.razor.cs
+++ BlazorAppOdataClient/Pages/Home.razor.cs
@@ -4,18 +4,39 @@ namespace BlazorAppOdataClient.Pages
 {
     public partial class Home
     {
-
+        ODataClient client = new ODataClient("https://localhost:7003/odata/");
         IEnumerable<Product> products;
         private async Task GetData()
         {
             try
             {
-                var client = new ODataClient("https://localhost:7003/odata/");
-
                 products = await client
                         .For<Product>()
                         .Expand(x => x.Brand)
                         .FindEntriesAsync();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private async Task GetDataWithId()
+        {
+            try
+            {
+                var product = await client
+                        .For<Product>()
+                        .Key(1)
+                        .Expand(x => x.Brand)
+                        .FindEntryAsync();
+
+                products = new List<Product>()
+                {
+                    product
+                };
 
             }
             catch (Exception ex)
